6051001d4f0cfea5ac0aceb11306e3c2b50edc71
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-05 21:21:44 -0400

    take defaults out of arguments

--- config.py
+++ config.py
@@ -14,14 +14,28 @@ def get_config(name):
     return configs[name]
 
 
+default = dict(
+    batch_size=20,
+    bptt=35,
+    clip=0.25,
+    dropout=0.2,
+    em_size=200,
+    lr=20,
+    n_head=2,
+    n_hid=200,
+    n_layers=2,
+)
+
 search = dict(
     batch_size=hp.choice("batch_size", [10, 20, 30]),
     bptt=hp.choice("bptt", [25, 35, 45]),
     clip=hp.choice("clip", [0.2, 0.25, 0.3]),
     dropout=hp.choice("dropout", [0.1, 0.2]),
+    em_size=hp.choice("em_size", [100, 200, 250]),
+    lr=hp.choice("lr", [10, 20, 30]),
     n_head=hp.choice("n_head", [1, 2, 3]),
-    em_size=hp.choice("n_hid", [100, 200]),
-    n_hid=hp.choice("n_layers", [1, 2, 3]),
+    n_hid=hp.choice("n_hid", [100, 200, 250]),
+    n_layers=hp.choice("n_layers", [1, 2, 3]),
 )
 
 configs = dict(search=search)
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import torch.onnx
 from ray import tune
 from ray.tune.suggest.hyperopt import HyperOptSearch
 
-from config import get_config
+from config import get_config, default
 from data import Corpus
 
 import models
@@ -20,16 +20,14 @@ import ours
 
 
 def add_arguments(parser):
-    parser.add_argument(
-        "--batch_size", type=int, default=20, metavar="N", help="batch size"
-    )
-    parser.add_argument("--bptt", type=int, default=35, help="sequence length")
+    parser.add_argument("--batch_size", type=int, metavar="N", help="batch size")
+    parser.add_argument("--bptt", type=int, help="sequence length")
     parser.add_argument("--cuda", action="store_true", help="use CUDA")
-    parser.add_argument("--clip", type=float, default=0.25, help="gradient clipping")
+    parser.add_argument("--clip", type=float, help="gradient clipping")
     parser.add_argument(
         "--config",
         type=get_config,
-        default=dict(),
+        default=default,
         help="config file to use for Experiment",
     )
     parser.add_argument(
@@ -48,15 +46,12 @@ def add_arguments(parser):
     parser.add_argument(
         "--dropout",
         type=float,
-        default=0.2,
         help="dropout applied to layers (0 = no dropout)",
     )
     parser.add_argument(
         "--dry-run", action="store_true", help="verify the code and the model"
     )
-    parser.add_argument(
-        "--em-size", type=int, default=200, help="size of word embeddings"
-    )
+    parser.add_argument("--em-size", type=int, help="size of word embeddings")
     parser.add_argument("--epochs", type=int, default=40, help="upper epoch limit")
     parser.add_argument(
         "--gpus-per-trial",
@@ -68,7 +63,7 @@ def add_arguments(parser):
     parser.add_argument(
         "--log-interval", type=int, default=200, metavar="N", help="report interval"
     )
-    parser.add_argument("--lr", type=float, default=20, help="initial learning rate")
+    parser.add_argument("--lr", type=float, help="initial learning rate")
     parser.add_argument(
         "--onnx-export",
         type=Path,
@@ -87,13 +82,10 @@ def add_arguments(parser):
     parser.add_argument(
         "--n-head",
         type=int,
-        default=2,
         help="the number of heads in the encoder/decoder of the transformer model",
     )
-    parser.add_argument(
-        "--n-hid", type=int, default=200, help="number of hidden units per layer"
-    )
-    parser.add_argument("--n-layers", type=int, default=2, help="number of layers")
+    parser.add_argument("--n-hid", type=int, help="number of hidden units per layer")
+    parser.add_argument("--n-layers", type=int, help="number of layers")
     parser.add_argument(
         "--n-samples",
         type=int,
