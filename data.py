94356f45a78f3e0b8052154171b6b0b6e7eb3f7a
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-06 15:01:52 -0400

    improve DebugDataset

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from io import open
 from pathlib import Path
+import numpy as np
 
 import torch
 from torch.utils.data import Dataset
@@ -68,16 +69,32 @@ class LMDataset(Dataset):
 
 
 class DebugDataset(Dataset):
-    def __init__(self, data: torch.Tensor, target: torch.Tensor):
+    def __init__(self, data: torch.Tensor, target: torch.Tensor, bptt: int):
+        self.bptt = bptt
         self.target = target
         self.data = data
 
+    @staticmethod
+    def generate_targets(data: np.ndarray, p):
+        vocab = 1 + data.max(initial=-np.inf)
+        mapping = np.random.choice(2, (vocab, vocab), p=[p, 1 - p]).astype(bool)
+        np.fill_diagonal(mapping, 0)
+        prev = [None for _ in range(vocab)]
+        for word in data:
+            yield prev[word] or vocab
+            for w in np.arange(vocab)[mapping[word]]:
+                prev[w] = word
+
+    def generate(self, path: Path, seed: int, size: int, n_tokens: int, p: float):
+        np.random.seed(seed)
+        data = np.random.choice(n_tokens, size)
+        target = list(self.generate_targets(data, p))
+        np.savez(str(path), data=data, target=target)
+
     def __getitem__(self, index):
-        seq_len = min(self.bptt, len(self.tokens) - 1 - index)
-        return (
-            self.tokens[index : index + seq_len],
-            self.tokens[index + 1 : index + seq_len + 1],
-        )
+        data = self.data[index : index + self.bptt]
+        target = self.target[index : index + self.bptt]
+        return data, target
 
     def __len__(self):
-        return len(self.tokens)
+        return len(self.target) - self.bptt
