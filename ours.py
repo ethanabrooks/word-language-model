65be941ae3df2e33fea6e3e7ab898eed9091c848
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-06 15:01:26 -0400

    fix bugs in ours.py

--- ours.py
+++ ours.py
@@ -14,28 +14,38 @@ def with_last_col_1(x: Tensor, last1=None):
     if last1 is None:
         last1 = torch.zeros_like(x)
         last1[..., -1] = 1
-    return last1 * (1 - x) + (1 - last1) * x
+    return x - x * last1 + last1
+
+
+def with_first_col_1(x: Tensor, first1=None):
+    if first1 is None:
+        first1 = torch.zeros_like(x)
+        first1[..., 0] = 1
+    return x - x * first1 + first1
 
 
 def scan(x: Tensor) -> Tensor:
     return with_last_col_1((1 - x).cumprod(-1)).roll(1, -1) * x
 
 
-def scan_in_time(x: Tensor) -> Tensor:
+def scan_forward(x: Tensor) -> Tensor:
+    x = with_last_col_1(x)
     x = torch.cat([row.roll(-i, -1) for i, row in enumerate(x.split(1, -2))], -2)
-    x = scan(with_last_col_1(x))
+    x = scan(x)
     return torch.cat([row.roll(i, -1) for i, row in enumerate(x.split(1, -2))], -2)
 
 
-class MultiheadAttention(multihead_attention.MultiheadAttention):
-    def __init__(self, embed_dim, num_heads, *args, **kwargs):
-        head_dim = embed_dim // num_heads
-        super().__init__(embed_dim, num_heads, *args, **kwargs)
-        self.connect_proj_weight = Parameter(torch.Tensor(head_dim, head_dim))
-        # self.linear = Linear(head_dim, 2)
-        xavier_uniform_(self.connect_proj_weight)
-        # xavier_uniform_(self.linear.weight)
+def scan_backward(x: Tensor) -> Tensor:
+    *_, n = x.shape
+    x = with_last_col_1(x.flip(-1))
+    n -= 1
+    x = torch.cat([row.roll(i - n, -1) for i, row in enumerate(x.split(1, -2))], -2)
+    x = scan(x)
+    x = torch.cat([row.roll(n - i, -1) for i, row in enumerate(x.split(1, -2))], -2)
+    return x.flip(-1)
 
+
+class MultiheadAttention(multihead_attention.MultiheadAttention):
     # noinspection PyPep8Naming
     def get_attn_output_weights(self, k, q):  # type: (Tensor, Tensor) -> Tensor
         r"""
@@ -57,24 +67,19 @@ class MultiheadAttention(multihead_attention.MultiheadAttention):
         """
         L = q.size(1)
         S = k.size(1)
-        forward_connections = (
-            q @ self.connect_proj_weight @ k.transpose(1, 2)
-        )  # (N, L, S)
-        backward_connections = (
-            q @ self.connect_proj_weight.T @ k.transpose(1, 2)
-        )  # (N, L, S)
-        scan_forward = scan_in_time(forward_connections)
-        scan_backward = scan_in_time(backward_connections.flip(-1)).flip(-1)
-        eye = torch.eye(L, S, device=q.device)
-        step_forward = eye.roll(1, -1)
-        step_backward = eye.roll(-1, -1)
-        step_forward[-1, 0] = 0
-        step_forward[-1, -1] = 1
-        step_backward[0, -1] = 0
-        step_backward[0, 0] = 1
+        connections = q @ k.transpose(1, 2)  # (N, L, S)
+        # forward_scan = scan_forward(connections)
+        backward_scan = scan_backward(connections)
+        # eye = torch.eye(L, S, device=q.device)
+        # step_forward = eye.roll(1, -1)
+        # step_backward = eye.roll(-1, -1)
+        # step_forward[-1, 0] = 0
+        # step_forward[-1, -1] = 1
+        # step_backward[0, -1] = 0
+        # step_backward[0, 0] = 1
         # softmax = self.linear(q).softmax(-1)
 
-        return scan_backward
+        return backward_scan
 
 
 class TransformerEncoderLayer(transformer.TransformerEncoderLayer):
