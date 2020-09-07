163f5181a3855fd6c2ef0cb9a6d20d4b0302fbf4
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-07 16:35:09 -0400

    add --last-col-1 and --forward-scan

--- multihead_attention.py
+++ multihead_attention.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import torch
 from torch.nn.functional import linear, softmax, dropout
+import torch.nn.functional as F
 from torch import Tensor
 from torch.nn import Module, Parameter
 from torch.nn.init import xavier_uniform_, constant_, xavier_normal_
