b0b0de32a3724bb6d80a1b3c34a97860d116381f
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-07 15:47:07 -0400

    allow running outside tune

--- train.py
+++ train.py
@@ -46,6 +46,7 @@ def run(
     n_head: int,
     n_hid: int,
     n_layers: int,
+    report: callable,
     save: Path,
     seed: int,
     tied: bool,
@@ -79,7 +80,7 @@ def run(
                 data, seed, n_seq=n_seq, seq_len=seq_len, n_tokens=n_tokens, p=p
             )
         dataset = DebugDataset(data)
-        assert bptt == dataset.bptt, f'set --bptt={dataset.bptt}.'
+        assert bptt == dataset.bptt, f"set --bptt={dataset.bptt}."
         n_seq = len(dataset)
         size_valid = int(n_seq * 0.2)
         size_test = int(n_seq * 0.1)
@@ -225,7 +226,7 @@ def run(
             for i, (info, mean_info, write_info) in enumerate(train()):
                 means.update(**mean_info)
                 if (i + 1) % log_interval == 0:
-                    tune.report(**info, **dict(means.items()))
+                    report(**info, **dict(means.items()))
                     with tune.checkpoint_dir(epoch) as path:
                         np.savez(
                             path,
@@ -236,7 +237,7 @@ def run(
                         )
 
             val_loss = np.mean(list(evaluate(val_data)))
-            tune.report(val_loss=val_loss, steps_since_save=steps_since_save)
+            report(val_loss=val_loss, steps_since_save=steps_since_save)
             if not best_val_loss or val_loss < best_val_loss:
                 with save.open("wb") as f:
                     torch.save(model, f)
@@ -261,7 +262,7 @@ def run(
 
     # Run on test data.
     test_loss = np.mean(list(evaluate(test_data)))
-    tune.report(test_loss=test_loss, test_ppl=math.exp(test_loss))
+    report(test_loss=test_loss, test_ppl=math.exp(test_loss))
 
     if onnx_export:
         # Export the model in ONNX format.
